8a624e53ade8d4a6a9c24b41868f354b171314ee
kusaline214 <dreamdumper@gmail.com>
2025-07-28 09:34:14 +0900

    全ページtitle修正

--- app/(page)/members/layout.tsx
+++ app/(page)/members/layout.tsx
@@ -2,7 +2,7 @@ import Hero from '@/app/_components/Hero';
 import Sheet from '@/app/_components/Sheet';
 
 export const metadata = {
-  title: 'メンバー｜シンプルなコーポレートサイト',
+  title: 'メンバー｜株式会社コードネスト',
 };
 
 type Props = {
