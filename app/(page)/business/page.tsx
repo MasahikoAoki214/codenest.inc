9cfd8a0e81d141c2caf7d977595ecb9b797f6588
kusaline214 <dreamdumper@gmail.com>
2025-07-28 10:41:34 +0900

    ビジネスページの不具合修正

--- app/(page)/business/page.tsx
+++ app/(page)/business/page.tsx
@@ -1,57 +1,61 @@
-import { NextRequest, NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import Image from 'next/image';
+import { getBusinessList } from '@/app/_libs/microcms';
+import styles from './page.module.css';
+import ButtonLink from '@/app/_components/ButtonLink';
 
-function validateEmail(email: string) {
-  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return pattern.test(email);
-}
+type Props = {
+  searchParams: {
+    dk: string;
+  };
+};
 
-export async function POST(request: NextRequest) {
-  const { lastname, firstname, company, email, message } = await request.json();
+export const revalidate = 60;
 
-  if (!lastname || !firstname || !company || !email || !message) {
-    return NextResponse.json(
-      { status: 'error', message: '全ての項目を入力してください' },
-      { status: 400 },
-    );
-  }
-
-  if (!validateEmail(email)) {
-    return NextResponse.json(
-      { status: 'error', message: 'メールアドレスの形式が誤っています' },
-      { status: 400 },
-    );
-  }
-
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"${lastname} ${firstname}" <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_TO,
-      subject: `【お問い合わせ】${company} - ${lastname} ${firstname}`,
-      text: `
-■ 姓: ${lastname}
-■ 名: ${firstname}
-■ 会社名: ${company}
-■ メールアドレス: ${email}
-■ メッセージ:
-${message}
-      `,
-    });
-
-    return NextResponse.json({ status: 'success', message: 'メールを送信しました' });
-  } catch (error) {
-    console.error('メール送信エラー:', error);
-    return NextResponse.json(
-      { status: 'error', message: 'メール送信に失敗しました' },
-      { status: 500 },
-    );
-  }
+export default async function Page({ searchParams }: Props) {
+  const data = await getBusinessList({
+    draftKey: searchParams.dk,
+  });
+  return (
+    <div className={styles.container}>
+      {data?.contents.length === 0 ? (
+        <p className={styles.empty}>事業内容が登録されていません。</p>
+      ) : (
+        <ul>
+          {data?.contents.map((business) => (
+            <li key={business.id} className={styles.list}>
+              <dl className={styles.flex}>
+                <dt className={styles.name}>
+                  <Image
+                    src={business.logo?.url as string}
+                    alt=""
+                    width={business.logo?.width}
+                    height={business.logo?.height}
+                    className={styles.logo}
+                  />
+                </dt>
+                <dd className={styles.description}>{business.description}</dd>
+                <dd className={styles.action}>
+                  <ButtonLink href={business.link} isExternal>
+                    サービスサイトへ
+                  </ButtonLink>
+                </dd>
+              </dl>
+              <Image
+                src={business.image?.url as string}
+                alt=""
+                width={business.image?.width}
+                height={business.image?.height}
+                className={styles.image}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className={styles.footer}>
+        <h2 className={styles.message}>We are hiring</h2>
+        <p>私たちは共にチャレンジする仲間を募集しています。</p>
+        <ButtonLink href="/recruit">採用情報へ</ButtonLink>
+      </div>
+    </div>
+  );
 }
